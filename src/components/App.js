5777946997c69c41f30892abf533f1a35c4d2814
Sohail Iqrar <sohailiqrar@gmail.com>
2023-02-27 16:09:47 +0530

    Added countdown component

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Card from "./Card";
 import Header from "./Header";
 import Footer from "./Footer";
+import Countdown from "./Countdown";
 
 function App() {
     return(
         <div>
             <Header />
+            <Countdown />
             <Card 
                 url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhNeT8M_A0M3IdLVU43dmzro4O8YSpSNI0iQ&usqp=CAU"
                 name="Chicken Bytes"
